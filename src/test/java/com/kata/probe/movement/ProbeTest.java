deb11849f01af145d471cc0bf2b87ec6d361e67d
Silvio Alberto <silvio.scarfo@bit.admin.ch>
2025-02-21 15:06:56 +0100

    Feature Kata Probe:
     - Added Parametrized Test for function turnLeft() and turnRight()

--- src/test/java/com/kata/probe/movement/ProbeTest.java
+++ src/test/java/com/kata/probe/movement/ProbeTest.java
@@ -3,6 +3,8 @@ package com.kata.probe.movement;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,16 +18,44 @@ class ProbeTest {
 
     @Nested
     class MoveTest {
-        @Test
-        void shouldTurnLeftFromNorthToWest() {
-            probe.move(Command.LEFT, null); // Grid not needed yet, use null
-            assertEquals(Direction.W, probe.getDirection());
+        @ParameterizedTest(name = "[{index}] argument is: {argumentsWithNames}")
+        @CsvSource(
+                useHeadersInDisplayName = true,
+                value = {
+                        "N, E",
+                        "E, S",
+                        "S, W",
+                        "W, N",
+                })
+        void shouldTurnToTheRightDirection(Direction startDirection, Direction finalDirection) {
+            //given
+            probe = new Probe(0, 0, startDirection);
+
+            //when
+            probe.move(Command.RIGHT, null); // Grid not needed yet, use null
+
+            //then
+            assertEquals(finalDirection, probe.getDirection());
         }
 
-        @Test
-        void shouldTurnRightFromNorthToEast() {
-            probe.move(Command.RIGHT, null); // Grid not needed yet, use null
-            assertEquals(Direction.E, probe.getDirection());
+        @ParameterizedTest(name = "[{index}] argument is: {argumentsWithNames}")
+        @CsvSource(
+                useHeadersInDisplayName = true,
+                value = {
+                        "N, W",
+                        "W, S",
+                        "S, E",
+                        "E, N",
+                })
+        void shouldTurnToTheLeftDirection(Direction startDirection, Direction finalDirection) {
+            //given
+            probe = new Probe(0, 0, startDirection);
+
+            //when
+            probe.move(Command.LEFT, null); // Grid not needed yet, use null
+
+            //then
+            assertEquals(finalDirection, probe.getDirection());
         }
     }
 
